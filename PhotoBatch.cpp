d730d173b6d196e88ce14a81f0768a64e6d3fe97
Gabriel <chavesmartinsg@gmail.com>
2022-06-16 17:57:20 -0300

    Adding constants to the name of the parameters

--- PhotoBatch.cpp
+++ PhotoBatch.cpp
@@ -2,6 +2,14 @@
 #include <locale.h>
 #include "ArgParser.h"
 
+#define RENAME_FLAG "rename"
+#define CONVERT_FLAG "convert"
+#define RESIZE_FLAG "resize"
+#define SCALE_FLAG "scale"
+
+#define FOLDER_OPTION "folder"
+#define AMOUNT_OPTION "amount"
+
 
 int main(int argc, char* argv[]){
 	setlocale(LC_ALL, "");
@@ -9,24 +17,24 @@ int main(int argc, char* argv[]){
 
 	ArgParser argparse;
 
-	argparse.RegisterFlag("rename");
-	argparse.RegisterFlag("convert");
-	argparse.RegisterFlag("resize");
-	argparse.RegisterFlag("scale");
+	argparse.RegisterFlag(RENAME_FLAG);
+	argparse.RegisterFlag(CONVERT_FLAG);
+	argparse.RegisterFlag(RESIZE_FLAG);
+	argparse.RegisterFlag(SCALE_FLAG);
 
-	argparse.RegisterOption("folder");
-	argparse.RegisterOption("amount");
+	argparse.RegisterOption(FOLDER_OPTION);
+	argparse.RegisterOption(AMOUNT_OPTION);
 
 	argparse.Parse(argc, argv);
 	
 
-	std::cout << std::boolalpha << "Rename\t" << argparse.GetFlag("rename") << std::endl;
-	std::cout << std::boolalpha << "Convert\t" << argparse.GetFlag("convert") << std::endl;
-	std::cout << std::boolalpha << "Resize\t" << argparse.GetFlag("resize") << std::endl;
-	std::cout << std::boolalpha << "Scale\t" << argparse.GetFlag("scale") << std::endl;
+	std::cout << std::boolalpha << "Rename\t" << argparse.GetFlag(RENAME_FLAG) << std::endl;
+	std::cout << std::boolalpha << "Convert\t" << argparse.GetFlag(CONVERT_FLAG) << std::endl;
+	std::cout << std::boolalpha << "Resize\t" << argparse.GetFlag(RESIZE_FLAG) << std::endl;
+	std::cout << std::boolalpha << "Scale\t" << argparse.GetFlag(SCALE_FLAG) << std::endl;
 
-	std::cout << std::boolalpha << "Folder= " << argparse.GetOptionAsString("folder") << std::endl;
-	std::cout << std::boolalpha << "Amount= " << argparse.GetOptionAsInt("amount") << std::endl;
+	std::cout << std::boolalpha << "Folder= " << argparse.GetOptionAsString(FOLDER_OPTION) << std::endl;
+	std::cout << std::boolalpha << "Amount= " << argparse.GetOptionAsInt(AMOUNT_OPTION) << std::endl;
 
 	std::cin.get();
 
