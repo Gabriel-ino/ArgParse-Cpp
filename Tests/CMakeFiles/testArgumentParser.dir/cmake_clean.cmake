22102c46c6b036844c1e3a9eb8801019695f97e6
Gabriel <chavesmartinsg@gmail.com>
2022-06-18 15:50:49 -0300

    Adding test framework

---
+++ Tests/CMakeFiles/testArgumentParser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testArgumentParser.dir/testArgumentParser.cpp.o"
+  "CMakeFiles/testArgumentParser.dir/testArgumentParser.cpp.o.d"
+  "testArgumentParser"
+  "testArgumentParser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testArgumentParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tests/CMakeFiles/testArgumentParser.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for testArgumentParser.
+# This may be replaced when dependencies are built.
